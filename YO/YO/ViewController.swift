9b6dde01419846501ba84fcd2170b82050953d33
Guy Kahlon <GuyKahlon@gmail.com>
2015-01-14 19:05:30 +0200

    Start Clone YO Project with Parse.com Swift and iOS 8

---
+++ YO/YO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YO
+//
+//  Created by Guy Kahlon on 1/14/15.
+//  Copyright (c) 2015 GuyKahlon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
